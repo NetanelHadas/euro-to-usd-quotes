7a1410a2e4fd2c6255ee1b5083f8dbebfac60182
DESKTOP-CT28P5M\netan <netanel.hadas@gmail.com>
2019-03-08 13:58:06 +0200

    rest api calls checked

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import React, { useState } from 'react';
 import ResourceList from './ResourceList';
 
 const App = () => {
-    const [resource, setResource] = useState('1 minute');
+    const [resource, setResource] = useState('MIN_1');
     return ( 
         <div>
             <div>
-                <button onClick={() => setResource('1 minute')}>1 minute</button>
-                <button onClick={() => setResource('5 minutes')}>5 minutes</button>
-                <button onClick={() => setResource('1 hour')}>1 hour</button>
-                <button onClick={() => setResource('1 week' )}>1 week</button>
+                <button onClick={() => setResource('MIN_1')}>1 minute</button>
+                <button onClick={() => setResource('MIN_5')}>5 minutes</button>
+                <button onClick={() => setResource('HOUR_1')}>1 hour</button>
+                <button onClick={() => setResource('WEEK_1')}>1 week</button>
             </div>
             <ResourceList resource={ resource }/>
         </div>
